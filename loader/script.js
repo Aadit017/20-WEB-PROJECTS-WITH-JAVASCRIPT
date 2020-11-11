48151fe786a6e97aea722c3897e6d09d12d5cf27
sachin <sachinarya@pop-os.localdomain>
2020-11-11 15:32:17 +0545

    CSS LOADING ANIMATION

---
+++ loader/script.js
@@ -0,0 +1,5 @@
+const loaderContainer = document.querySelector('.loader-container');
+window.addEventListener('load',()=>{
+    loaderContainer.classList.add('hide-loader');
+    document.body.classList.add('show-oveflow');
+});
